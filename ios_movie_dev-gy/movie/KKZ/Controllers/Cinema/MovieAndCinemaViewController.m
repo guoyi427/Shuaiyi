f46bf129f769647c3a7fc7e7a880053b3032ecfa
guoyi <guoyi@kokozu.net>
2017-11-20 17:48:36 +0800

    cinema list district

--- ios_movie_dev-gy/movie/KKZ/Controllers/Cinema/MovieAndCinemaViewController.m
+++ ios_movie_dev-gy/movie/KKZ/Controllers/Cinema/MovieAndCinemaViewController.m
@@ -23,6 +23,9 @@
 #import "CityListNewViewController.h"
 #import "CinemaSearchViewController.h"
 
+//  Model
+#import "CinemaHelper.h"
+
 static CGFloat segmentedControlWidth = 150.0f;
 static CGFloat topBarHeight = 35.0f;
 
@@ -33,11 +36,18 @@ static NSString *CinemaCellIdentifier = @"Cinema-cell";
 #define KOKOLOCATIONVIEWWIDTH ((screentWith - 158) * 0.5 - 10)
 #define KOKOLOCATIONVIEWHEIGHT 44.0f
 
-@interface MovieAndCinemaViewController () <KOKOLocationViewDelegate, CityListViewControllerDelegate, UITableViewDelegate, UITableViewDataSource>
+@interface MovieAndCinemaViewController () <KOKOLocationViewDelegate, CityListViewControllerDelegate, UITableViewDelegate, UITableViewDataSource, VerticalCinemaPickerViewDelegate>
 {
     //  UI
     KOKOLocationView *_locationView;
     UISegmentedControl *_segmentedControl;
+    UIButton *_selectDistrictBtn;
+    //遮层
+    UIControl *ctrHolder;
+    //遮层
+    UIControl *ctrHolderWhite;
+    //区域选择view
+    VerticalCinemaPickerView *districtView;
     
     UIView *_topBarView;
     UIButton *_currentMovieButton;
@@ -51,6 +61,9 @@ static NSString *CinemaCellIdentifier = @"Cinema-cell";
     NSMutableArray *_currentMovieList;
     NSMutableArray *_futureMovieList;
     NSMutableArray *_cinemaList;
+    NSMutableArray *_networkCinemaList;
+    NSMutableArray *_districtList;
+    NSMutableArray *_districtCinemaList;
 }
 @end
 
@@ -114,6 +127,20 @@ static NSString *CinemaCellIdentifier = @"Cinema-cell";
     [searchButton setImage:[UIImage imageNamed:@"searchIcon"] forState:UIControlStateNormal];
     [searchButton addTarget:self action:@selector(searchButtonAction:) forControlEvents:UIControlEventTouchUpInside];
     [self.navBarView addSubview:searchButton];
+    
+    //右边筛选按钮
+    _selectDistrictBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    _selectDistrictBtn.backgroundColor = [UIColor clearColor];
+    _selectDistrictBtn.hidden = true;
+    _selectDistrictBtn.frame = CGRectMake(screentWith - 44 - 5 - 44, 20, 44, 44);
+    _selectDistrictBtn.contentEdgeInsets = UIEdgeInsetsMake(13, 10, 13, 12);
+    
+    [_selectDistrictBtn addTarget:self
+                          action:@selector(showDistrictView)
+                forControlEvents:UIControlEventTouchUpInside];
+    [_selectDistrictBtn setImage:[UIImage imageNamed:@"search_cinemaDistrict_btn"]
+                       forState:UIControlStateNormal];
+    [self.navBarView addSubview:_selectDistrictBtn];
     
     _segmentedControl = [[UISegmentedControl alloc] initWithItems:@[@"电影", @"影院"]];
     _segmentedControl.frame = CGRectMake((kAppScreenWidth - segmentedControlWidth)/2.0,30, segmentedControlWidth, 44-20);
@@ -263,6 +290,13 @@ static NSString *CinemaCellIdentifier = @"Cinema-cell";
                                [_cinemaList removeAllObjects];
                                [_cinemaList addObjectsFromArray:sortedCinemas];
                                [_cinemaTableView reloadData];
+                               _networkCinemaList = [sortedCinemas mutableCopy];
+                               
+                               //按城区分组
+                               [CinemaHelper groupDistrict:_networkCinemaList finish:^(NSArray *districts, NSArray *districtsCinemas) {
+                                    _districtCinemaList = [districtsCinemas mutableCopy];
+                                    _districtList = [districts mutableCopy];
+                                }];
                            }
                            failure:^(NSError *_Nullable err) {
                                DLog(@"请求影院列表 失败 %@", err);
@@ -293,11 +327,13 @@ static NSString *CinemaCellIdentifier = @"Cinema-cell";
         _currentMovieTableView.hidden = !_currentMovieButton.isSelected;
         _futureMovieTableView.hidden = !_futureMovieButton.isSelected;
         _cinemaTableView.hidden = true;
+        _selectDistrictBtn.hidden = true;
     } else {
         //  影院
         _currentMovieTableView.hidden = true;
         _futureMovieTableView.hidden = true;
         _cinemaTableView.hidden = false;
+        _selectDistrictBtn.hidden = false;
     }
     
     _topBarView.hidden = segmented.selectedSegmentIndex == 1;
@@ -328,6 +364,109 @@ static NSString *CinemaCellIdentifier = @"Cinema-cell";
     _currentMovieTableView.hidden = true;
     _futureMovieTableView.hidden = false;
     _cinemaTableView.hidden = true;
+}
+
+/**
+ MARK:点击区域检索
+ */
+- (void)showDistrictView {
+    
+    //统计事件：影院列表筛选
+    StatisEvent(EVENT_CINEMA_FILTER);
+    
+    ctrHolder =
+    [[UIControl alloc] initWithFrame:CGRectMake(0, 44 + self.contentPositionY,
+                                                screentWith, screentHeight)];
+    [ctrHolder addTarget:self
+                  action:@selector(hiddenDistrictView)
+        forControlEvents:UIControlEventTouchUpInside];
+    ctrHolder.backgroundColor = [UIColor blackColor];
+    ctrHolder.alpha = 0.5;
+    
+    ctrHolderWhite = [[UIControl alloc]
+                      initWithFrame:CGRectMake(0, 0, screentWith, 44 + self.contentPositionY)];
+    [ctrHolderWhite addTarget:self
+                       action:@selector(hiddenDistrictView)
+             forControlEvents:UIControlEventTouchUpInside];
+    ctrHolderWhite.backgroundColor = [UIColor whiteColor];
+    ctrHolderWhite.alpha = 0.1;
+    
+    districtView = [[VerticalCinemaPickerView alloc]
+                    initWithFrame:CGRectMake(0, 44 + self.contentPositionY, screentWith,
+                                             337)];
+    districtView.districtList = _districtList;
+    districtView.allCinemasNum = _cinemaList.count;
+    NSMutableDictionary *disDic =
+    [NSMutableDictionary dictionaryWithCapacity:_districtList.count];
+    //生成城区和个数dic
+    for (NSInteger i = 0; i < _districtList.count; i++) {
+        NSString *disName = _districtList[i];
+        NSArray *cinemas = [_districtCinemaList objectAtIndex:i];
+        [disDic setObject:[NSNumber numberWithUnsignedInteger:cinemas.count]
+                   forKey:disName];
+    }
+    districtView.districtDict = [disDic copy];
+    districtView.delegate = self;
+    
+    UIWindow *keywindow = [[UIApplication sharedApplication] keyWindow];
+    [keywindow addSubview:ctrHolder];
+    [keywindow addSubview:ctrHolderWhite];
+    [keywindow addSubview:districtView];
+    
+    [districtView updateLayout];
+    
+    districtView.frame =
+    CGRectMake(0, 44 + self.contentPositionY, screentWith, 0);
+    districtView.cinemaTable.frame = CGRectMake(110, 0, screentWith - 110, 0);
+    
+    [UIView animateWithDuration:0.3
+                     animations:^{
+                         
+                         districtView.frame =
+                         CGRectMake(0, 44 + self.contentPositionY, screentWith, 337);
+                         districtView.cinemaTable.frame =
+                         CGRectMake(110, 0, screentWith - 110, 337);
+                         
+                     }
+                     completion:^(BOOL finished){
+                     }];
+}
+
+- (void)hiddenDistrictView {
+    
+    [UIView animateWithDuration:0.5
+                     animations:^{
+                         
+                         districtView.frame =
+                         CGRectMake(0, 44 + self.contentPositionY, screentWith, 337);
+                         
+                     }
+                     completion:^(BOOL finished) {
+                         
+                         [districtView removeFromSuperview];
+                         
+                         [ctrHolder removeFromSuperview];
+                         [ctrHolderWhite removeFromSuperview];
+                         
+                     }];
+}
+
+/**
+ MARK: 城区回调
+ 
+ @param districtIndex index
+ */
+- (void)selectDistrictName:(NSInteger)districtIndex {
+    if (districtIndex == 0) {
+        //全部影院
+        _cinemaList = [_networkCinemaList mutableCopy];
+    } else {
+        //某个城区影院
+        _cinemaList = [_districtCinemaList[districtIndex - 1] mutableCopy];
+    }
+    
+    [_cinemaTableView reloadData];
+    [self hiddenDistrictView];
 }
 
 #pragma mark - KOKOLocationView - Delegate
